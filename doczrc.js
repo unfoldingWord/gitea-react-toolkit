2ab250a66ef3bc44400dd6b8a804f65414e4bee4
Christopher Klapp <christopher@klapp.name>
2019-04-11 20:16:24 -0400

    fix netlify routes

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,4 @@
+
 export default {
   menu: [
     'Getting Started',
---
+++ scripts/docz-netlify.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+const fs = require("fs");
+
+fs.writeFileSync(`./.docz/dist/_redirects`, '/*    /index.html   200', {
+  encoding: 'utf8'
+});
+
+console.log('done');
