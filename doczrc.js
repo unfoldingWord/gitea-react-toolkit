e95bc96e3d5df375bed6685e9e2453d832ec0060
RoyalSix <jay.a.scottjr@biola.edu>
2019-10-23 08:50:45 -0700

    added types to core.ts

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,6 @@
 
 export default {
+  typescript: true,
   menu: [
     'Getting Started',
     'ApplicationBar',
--- src/core/gitea-api/authentication.js
+++ src/core/gitea-api/authentication.js
@@ -1,24 +1,44 @@
 import base64 from 'base-64';
 import utf8 from 'utf8';
 
-import { getUser, getTokens, createToken } from './users';
+import {
+  getUser, getTokens, createToken,
+} from './users';
 
-export const authenticate = async ({username='', password='', config}) => {
-  let authentication = {config};
-  if (username) authentication.user = await getUser({username, config});
+export const authenticate = async ({
+  username = '', password = '', config,
+}) => {
+  let authentication = { config };
+
+  if (username) {
+    authentication.user = await getUser({ username, config });
+  }
+
   if (username && password) {
-    const tokens = await getTokens({username, password, config});
+    const tokens = await getTokens({
+      username, password, config,
+    });
+
     if (tokens) {
       const tokenMatches = tokens.filter(_token => _token.name === config.tokenid);
-      if (tokenMatches.length > 0) authentication.token = tokenMatches[0];
-      else authentication.token = await createToken({username, password, config});
+
+      if (tokenMatches.length > 0) {
+        authentication.token = tokenMatches[0];
+      } else {
+        authentication.token = await createToken({
+          username, password, config,
+        });
+      }
     }
   }
   return authentication;
 };
 
-export const encodeAuthentication = ({username, password, token}) => {
+export const encodeAuthentication = ({
+  username, password, token,
+}) => {
   let authentication;
+
   if (token) {
     let sha1 = typeof token === 'object' ? token.sha1 : token;
     authentication = `token ${sha1}`;
