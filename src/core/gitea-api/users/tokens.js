59878cf82c5d88cbe243a07f5f830a409eb03bbc
Christopher Klapp <christopher@klapp.name>
2019-10-23 21:32:29 -0400

    tokens: don't edit config

--- src/core/gitea-api/users/tokens.js
+++ src/core/gitea-api/users/tokens.js
@@ -7,12 +7,12 @@ export const getTokens = async ({username, password, config}) => {
   let tokens;
   const url = Path.join(apiPath, 'users', username, 'tokens');
   const authentication = encodeAuthentication({username, password});
-  config.headers = {
+  const headers = {
       'Content-Type': 'application/json',
       'Authorization': authentication,
   };
   try {
-    tokens = await get({url, config});
+    tokens = await get({url, config: {...config, headers}});
   } catch { tokens = null; }
   return tokens;
 };
@@ -21,13 +21,13 @@ export const createToken = async ({username, password, config={}}) => {
   let token;
   const url = Path.join(apiPath, 'users', username, 'tokens');
   const authentication = encodeAuthentication({username, password});
-  config.headers = {
+  const headers = {
     'Content-Type': 'application/json',
     'Authorization': authentication,
   };
   const payload = {"name": config.tokenid};
   try {
-    token = await post({url, payload, config});
+    token = await post({url, payload, config: {...config, headers}});
   } catch { token = []; }
   return token;
 };
