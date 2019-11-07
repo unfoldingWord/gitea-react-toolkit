114549c5d2d847cd24d27909a0fdaa87b48f2cd2
RoyalSix <jay.a.scottjr@biola.edu>
2019-11-07 15:16:58 -0800

    added cypress log

--- src/components/authentication/authentication.spec.js
+++ src/components/authentication/authentication.spec.js
@@ -2,6 +2,7 @@
 describe('Authentication', function () {
   it('should not login with incorrect password', function () {
     cy.visit('/authentication');
+    console.log(Cypress.env());
     cy.get('[data-test=username-input] input').eq(0).type(Cypress.env('TEST_USERNAME'));
     cy.get('[data-test=password-input] input').eq(0).type(Cypress.env('TEST_PASSWORD'));
     cy.get('[data-test=remember-checkbox]').eq(0).click();
