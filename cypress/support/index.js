6e2044d4761edbdf42bd2bb9e8f199fbee6904b5
RoyalSix <jay.a.scottjr@biola.edu>
2019-11-08 09:12:29 -0800

    test en

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+
+Cypress.Screenshot.defaults({ screenshotOnRunFailure: false });
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
