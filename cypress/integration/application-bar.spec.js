23eb4a2b97bf1afa4a0f7e70ced49e56e007625e
RoyalSix <jay.a.scottjr@biola.edu>
2019-11-25 11:32:55 -0800

    removed waiting for XHR request

--- cypress/integration/application-bar.spec.js
+++ cypress/integration/application-bar.spec.js
@@ -15,8 +15,6 @@ describe('Application Bar', function () {
     cy.contains('Door43');
     cy.get('[data-test=repository-menu-modal] [data-test=repository-item]').eq(0).click();
     cy.get('[data-test=drawer-menu-button]').eq(0).click();
-    cy.server().route('GET', 'https://git.door43.org/api/v1/repos/Door43-Catalog/**').as('tree');
-    cy.wait('@tree').its('status').should('be', 200);
     cy.get('[data-test=file-tree]', { timeout: 10000 }).children().should('have.length', 9);
     cy.get('[data-test=drawer-menu-close-button]').click();
     cy.get('[data-test=application-bar]').should('contain.text', 'en_ta');
