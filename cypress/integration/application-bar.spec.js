37754da3348fe56f503d33f97dda9c73ff0b22a3
RoyalSix <jay.a.scottjr@biola.edu>
2019-11-25 10:24:42 -0800

    added tests for auth functionality in application bar

--- cypress/integration/application-bar.spec.js
+++ cypress/integration/application-bar.spec.js
@@ -3,9 +3,11 @@ describe('Application Bar', function () {
   before(() => {
     cy.clearLocalStorage();
   });
+  beforeEach(() => {
+    cy.visit('/application-bar');
+  })
 
   it('should test basic functionality of application bar with repository selection', function () {
-    cy.visit('/application-bar');
     cy.get('[data-test=application-bar]').should('not.contain.text', 'en_ta');
     cy.get('[data-test=repository-menu]').eq(0).click();
     cy.wait(1000);
@@ -20,6 +22,18 @@ describe('Application Bar', function () {
     cy.get('[data-test=application-bar]').should('contain.text', 'en_ta');
     cy.get('[data-test=repository-item-icon] svg').click();
     cy.get('[data-test=application-bar]').should('not.contain.text', 'en_ta');
+  });
+
+  it('should test signing in from application bar', () => {
+    cy.get('[data-test=user-menu-icon]').eq(0).click();
+    cy.get('[data-test=user-menu-icon] img').should('not.exist');
+    cy.get('[data-test=username-input] input').eq(0).type(Cypress.env('TEST_USERNAME'));
+    cy.get('[data-test=password-input] input').eq(0).type(Cypress.env('TEST_PASSWORD'));
+    cy.get('[data-test=remember-checkbox]').eq(0).click();
+    cy.get('[data-test=submit-button]').eq(0).click();
+    cy.contains('Logout');
+    cy.get('body').click('top')
+    cy.get('[data-test=user-menu-icon] img').should('exist');
   });
 });
 
