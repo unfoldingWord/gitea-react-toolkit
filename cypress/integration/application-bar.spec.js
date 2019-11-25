60a43286e170a6e2b4c96766cf151f31228666ba
RoyalSix <jay.a.scottjr@biola.edu>
2019-11-25 09:10:58 -0800

    added better query selector assertions

--- cypress/integration/application-bar.spec.js
+++ cypress/integration/application-bar.spec.js
@@ -3,20 +3,18 @@ describe('Application Bar', function () {
   before(() => {
     cy.clearLocalStorage();
   });
+
   const numberOfDefaultItems = 4;
+
   it('should not login with incorrect password', function () {
     cy.visit('/application-bar');
     cy.get('[data-test=repository-menu]').eq(0).click();
-    cy.server().route("GET", 'https://git.door43.org/api/v1/repos/**').as('dataGetFirst');
-    cy.wait(Array(numberOfDefaultItems).fill('@dataGetFirst')).its('status').should('be', 200);
-    cy.get('[data-test=repository-menu-modal] [data-test=repository-item]').as('repository-items');
-    cy.get('@repository-items').then(($items) => {
-      expect($items.length).eq(numberOfDefaultItems);
-    });
+    cy.wait(1000);
+    cy.get('[data-test=repository-menu-modal] [data-test=repository-item]', { timeout: 10000 }).should('have.length', numberOfDefaultItems)
     cy.contains('Door43');
     cy.get('[data-test=repository-menu-modal] [data-test=repository-item]').eq(0).click();
     cy.get('[data-test=drawer-menu-button]').eq(0).click();
-    cy.server().route("GET", 'https://git.door43.org/api/v1/repos/Door43-Catalog/**').as('tree');
+    cy.server().route('GET', 'https://git.door43.org/api/v1/repos/Door43-Catalog/**').as('tree');
     cy.wait('@tree').its('status').should('be', 200);
     cy.get('[data-test=file-tree]', { timeout: 10000 }).children().should('have.length', 9);
   });
