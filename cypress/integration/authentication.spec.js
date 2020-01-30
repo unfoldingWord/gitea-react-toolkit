786675183989bdc15faa42de7efb1d71dff55ad4
RoyalSix <jay.a.scottjr@biola.edu>
2020-01-30 09:34:53 -0800

    fixed test

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -3,7 +3,7 @@ describe('Authentication', function () {
   before(() => {
     cy.clearLocalStorage();
   });
-  it('should not login with incorrect password', function () {
+  it('should login with correct password', function () {
     cy.visit('/authentication');
     cy.get('[data-test=username-input] input').eq(0).type(Cypress.env('TEST_USERNAME'));
     cy.get('[data-test=password-input] input').eq(0).type(Cypress.env('TEST_PASSWORD'));
