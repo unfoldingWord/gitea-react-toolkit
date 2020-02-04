af0fb56a7d808d5e4c8bdf13e0db4e2c3b143387
RoyalSix <jay.a.scottjr@biola.edu>
2020-02-04 11:02:00 -0800

    fixed spacing

--- src/core/gitea-api/users/users.ts
+++ src/core/gitea-api/users/users.ts
@@ -1,17 +1,21 @@
 import Path from 'path';
 import { APIConfig } from '../core.d';
-import { apiPath, get, ERROR_SERVER_UNREACHABLE, ERROR_NETWORK_DISCONNECTED } from '../core';
+import {
+  apiPath, get, ERROR_SERVER_UNREACHABLE, ERROR_NETWORK_DISCONNECTED,
+} from '../core';
 
 export const getUser = async ({ username, config }: {
   username: string;
   config: APIConfig;
-}): Promise<{ id: object; } | null> => {
+}): Promise<{ id: object } | null> => {
   let user = null;
   const url = Path.join(apiPath, 'users', username);
+
   try {
     user = await get({ url, config });
   } catch (e) {
     const errorMessage = e && e.message ? e.message : '';
+
     if (errorMessage.match(ERROR_SERVER_UNREACHABLE) || errorMessage.match(ERROR_NETWORK_DISCONNECTED)) {
       throw e;
     }
