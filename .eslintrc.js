aefd1265942dc0eaca5a210d0bc85b6b672c6741
RoyalSix <jay.a.scottjr@biola.edu>
2020-03-10 08:40:41 -0700

    added ts fixes for build

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,36 +4,65 @@ module.exports = {
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
-    "@unfoldingword"
+    "@unfoldingword",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript"
   ],
   "plugins": [
     "react",
-    "cypress",
-    "chai-friendly"
+    "chai-friendly",
+    "@typescript-eslint"
   ],
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true
     },
     "ecmaVersion": 7,
     "sourceType": "module",
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname
+  },
+  "rules": {
+    "no-unused-vars": "warn",
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2,
+    "no-nested-ternary": ["off"],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/typedef": ["off"],
+    "@typescript-eslint/no-unnecessary-type-arguments": ["off"],
+    "@typescript-eslint/no-this-alias": ["off"],
+    "@typescript-eslint/no-non-null-assertion": ["off"],
+    "@typescript-eslint/no-namespace": ["off"],
+    "@typescript-eslint/no-misused-new": ["off"],
+    "@typescript-eslint/no-misused-promises": ["off"],
+    "@typescript-eslint/no-magic-numbers": ["off"],
+    "@typescript-eslint/no-floating-promises": ["off"],
+    "@typescript-eslint/func-call-spacing": ["off"],
+    "@typescript-eslint/no-inferrable-types": ["off"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/adjacent-overload-signatures": ["off"],
+    "@typescript-eslint/consistent-type-definitions": ["off"],
+    "@typescript-eslint/class-name-casing": ["off"],
+    "@typescript-eslint/camelcase": ["off"],
+    "@typescript-eslint/no-misused-promises": ["off"],
+    "camelcase": ["warn"],
+    "no-undefined": "warn",
+    "semi": ["error", "always", { "omitLastInOneLineBlock": true }],
+    "no-console": "off",
+    "indent": ["error", 2, {
+      "SwitchCase": 1,
+      "FunctionExpression": { "parameters": "first" },
+      "FunctionDeclaration": { "parameters": "first" },
+      "CallExpression": { "arguments": "first" }
+    }]
   },
   "overrides": [
     {
+      // enable the rule specifically for TypeScript files
       "files": ["*.ts", "*.tsx"],
-      "parser": "@typescript-eslint/parser",
-      "extends": [
-        "plugin:@typescript-eslint/recommended"
-      ],
-      "plugins": [
-        "@typescript-eslint"
-      ],
-      "parserOptions": {
-        project: path.resolve(__dirname, './tsconfig.json'),
-        tsconfigRootDir: __dirname
-      },
       "rules": {
-        "@typescript-eslint/typedef": 2,
+        "@typescript-eslint/explicit-function-return-type": ["error", { "allowExpressions": true, "allowTypedFunctionExpressions": true }],
         "@typescript-eslint/no-explicit-any": ["off"],
         "@typescript-eslint/typedef": [1, { "parameter": true, "arrowParameter": false }],
         "@typescript-eslint/no-unnecessary-type-arguments": ["error"],
@@ -60,28 +89,10 @@ module.exports = {
       }
     }
   ],
-  "rules": {
-    "curly": ["error", "multi", "consistent"],
-    "no-unused-vars": "warn",
-    "no-unused-expressions": 0,
-    "chai-friendly/no-unused-expressions": 2,
-    "no-nested-ternary": ["off"],
-    "camelcase": ["warn"],
-    "no-undefined": "warn",
-    "semi": ["error", "always", { "omitLastInOneLineBlock": true }],
-    "no-console": "off",
-    "indent": ["error", 2, {
-      "SwitchCase": 1,
-      "FunctionExpression": { "parameters": "first" },
-      "FunctionDeclaration": { "parameters": "first" },
-      "CallExpression": { "arguments": "first" }
-    }]
-  },
   "env": {
     "browser": true,
     "node": true,
-    "jest": true,
-    "cypress/globals": true
+    "jest": true
   },
   "root": true
 }
