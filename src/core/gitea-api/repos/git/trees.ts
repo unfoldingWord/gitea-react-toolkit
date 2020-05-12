233a3ebb36153b2ee8b5a3ef28a29c98a5e06264
RoyalSix <jay.a.scottjr@biola.edu>
2020-05-12 10:10:48 -0700

    added fix for path.join undefined

--- src/core/gitea-api/repos/git/trees.ts
+++ src/core/gitea-api/repos/git/trees.ts
@@ -16,25 +16,33 @@ export const getFullTree = async ({
   sha,
   config,
   recursive=true,
-  per_page=10000
+  per_page=10000,
 }) => {
   let tree = [];
   let moreData = true;
   let page = 0;
+
   while (moreData) {
-    const options = { recursive, per_page, page };
-    const data = await fetchTree({owner, repository, sha, config, ...options});
+    const options = {
+      recursive, per_page, page,
+    };
+    const data = await fetchTree({
+      owner, repository, sha, config, ...options,
+    });
     const _tree = data.tree.map(blob => {
       const getBlob = async () => {
-        const _blob = await get({url: blob.url, config});
+        const _blob = await get({ url: blob.url, config });
         const decoded = (_blob.encoding === 'base64') ? decodeBase64ToUtf8(_blob.content) : undefined;
-        return {..._blob, decoded};
+        return { ..._blob, decoded };
       };
-      return  {...blob, getBlob};
+      return { ...blob, getBlob };
     });
     tree = tree.concat(_tree);
     moreData = data.truncated;
-    if (moreData) page ++;
+
+    if (moreData) {
+      page ++;
+    }
   }
   return tree;
 };
@@ -46,15 +54,23 @@ export const decodeBase64ToUtf8 = (encoded) => {
 };
 
 // http://bg.door43.org/api/v1/repos/unfoldingword/en_ugl/git/trees/master
-export const fetchTree = async ({ owner, repository, sha='master', per_page, page, recursive, config }) => {
-  const params = {recursive, per_page, page};
+export const fetchTree = async ({
+  owner, repository, sha='master', per_page, page, recursive, config,
+}) => {
+  const params = {
+    recursive, per_page, page,
+  };
   const url = path.join(apiPath, 'repos', owner, repository, 'git', 'trees', sha);
-  const data = await get({ url, params: params, config });
+  const data = await get({
+    url, params: params, config,
+  });
   return data;
 };
 
 // /api/v1/repos/unfoldingWord/en_ta/git/trees/master
-export const repoTreeUrl = ({ full_name, branch, default_branch }) => {
+export const repoTreeUrl = ({
+  full_name, branch = '', default_branch = '',
+}) => {
   const url = path.join(apiPath, 'repos', full_name, 'git', 'trees', branch || default_branch);
   return url;
 };
