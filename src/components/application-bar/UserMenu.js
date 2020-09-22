cc95118351902b455fb5c4eaa99994cf05b215fe
mannycolon <colonmanuel7@gmail.com>
2020-09-22 12:53:26 -0400

    Bug fixes

--- src/components/application-bar/UserMenu.js
+++ src/components/application-bar/UserMenu.js
@@ -21,7 +21,7 @@ function UserMenu() {
     if (authentication && authentication.user) {
       closeModal();
     }
-  }, [authentication])
+  }, [authentication.user])
 
   const avatar = !(authentication && authentication.user) ? <AccountCircle fontSize="large" /> : (
     <Avatar data-test="user-menu-avatar" className={classes.avatar} src={authentication.user.avatar_url} />
