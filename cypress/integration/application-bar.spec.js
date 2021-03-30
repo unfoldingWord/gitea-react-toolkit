f0b80dbed7fcbfae219c5e7854c53fcd83260bcf
Bruce McLean <mcleanb@photo-nomad.net>
2021-03-30 15:35:27 -0400

    fix cypress tests - adding delays for screen updates

--- cypress/integration/application-bar.spec.js
+++ cypress/integration/application-bar.spec.js
@@ -15,12 +15,15 @@ describe('Application Bar', function () {
     cy.get('[data-test=repository-menu-modal] [data-test=repository-item]', { timeout: 10000 }).should('have.length', 4);
     cy.contains('Door43');
     cy.get('[data-test=repository-menu-modal] [data-test=repository-item]').eq(0).click();
-    cy.wait(500); // add delay for screen update
+    cy.wait(100);
     cy.get('[data-test=drawer-menu-button]').eq(0).click();
+    cy.wait(500);
     cy.get('[data-test=file-tree]', { timeout: 10000 }).children().should('have.length', 8);
     cy.get('[data-test=drawer-menu-close-button]').click();
+    cy.wait(100);
     cy.get('[data-test=application-bar]').should('contain.text', 'en_ta');
     cy.get('[data-test=repository-item-icon] svg').click();
+    cy.wait(100);
     cy.get('[data-test=application-bar]').should('not.contain.text', 'en_ta');
   });
 
