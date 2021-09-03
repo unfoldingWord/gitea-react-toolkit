3f2a646d96d3ffabeee8fbcd1372036538c3439a
rady-ben <radhouanebenbrahim@gmail.com>
2021-09-03 22:32:19 +0100

    configure enzyme and test the render of FileForm

--- __mocks__/App.test.js
+++ __mocks__/App.test.js
@@ -1,6 +1,16 @@
 import React from "react";
-import ApplicationBar from '../src/components/application-bar/ApplicationBar';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import FileForm from '../src/components/file/FileForm';
+FileForm
+
+Enzyme.configure({
+    adapter: new Adapter(),
+})
 
 test('render without error', () => {
-
+    const wrapper = shallow(<FileForm 
+        submitText="submitText"
+        onSubmit={()=>{}}
+    />);
 });
