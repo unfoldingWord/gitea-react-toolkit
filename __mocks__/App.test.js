b7b096070677a53c889670372d43cf982af32072
rady-ben <radhouanebenbrahim@gmail.com>
2021-09-05 07:02:58 +0100

    remove unused componenent

--- __mocks__/App.test.js
+++ __mocks__/App.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import FileForm from '../src/components/file/FileForm';
-FileForm
 
 Enzyme.configure({
     adapter: new Adapter(),
