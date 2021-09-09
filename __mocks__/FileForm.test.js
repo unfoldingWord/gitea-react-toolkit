2ffcbde0162885d42ef6880b9139e302a6b05e72
rady-ben <radhouanebenbrahim@gmail.com>
2021-09-09 18:58:18 +0100

    refractor the Enzyme utils in separate file to not repeat the config in each test fine

--- __mocks__/FileForm.test.js
+++ __mocks__/FileForm.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import checkPropTypes from 'check-prop-types';
 import FileForm from '../src/components/file/FileForm';
+import { findByAttribute, checkProps } from './testUtils';
 
 Enzyme.configure({
     adapter: new Adapter(),
@@ -22,50 +22,49 @@ jest.mock('react', () => ({
     useState: (initialState) => [initialState, mockSetBranch]
 }))
 
-const findByAttribute = (wrapper, attribute) => wrapper.find(`[data-test='${attribute}']`)
-
 // render tests
-test('render fileForm', () => {
-    const wrapper = setupWrapper();
-    const fileForm = findByAttribute(wrapper, 'component-fileForm');
-    expect(fileForm.length).toBe(1);
-});
-
-test('render branch-textField', () => {
-    const wrapper = setupWrapper();
-    const branch = findByAttribute(wrapper, 'branch-textField');
-    expect(branch.length).toBe(1);
-});
-
-test('render filepath-textField', () => {
-    const wrapper = setupWrapper();
-    const filepath = findByAttribute(wrapper, 'filepath-textField');
-    expect(filepath.length).toBe(1);
-});
-
-test('render defaultContent-textField', () => {
-    const wrapper = setupWrapper();
-    const defaultContent = findByAttribute(wrapper, 'defaultContent-textField');
-    expect(defaultContent.length).toBe(1);
-});
-
-test('render submit-button', () => {
-    const wrapper = setupWrapper();
-    const submit = findByAttribute(wrapper, 'submit-button');
-    expect(submit.length).toBe(1);
+describe('render fileForm elements',() => {
+    test('render fileForm', () => {
+        const wrapper = setupWrapper();
+        const fileForm = findByAttribute(wrapper, 'component-fileForm');
+        expect(fileForm.length).toBe(1);
+    });
+    
+    test('render branch-textField', () => {
+        const wrapper = setupWrapper();
+        const branch = findByAttribute(wrapper, 'branch-textField');
+        expect(branch.length).toBe(1);
+    });
+    
+    test('render filepath-textField', () => {
+        const wrapper = setupWrapper();
+        const filepath = findByAttribute(wrapper, 'filepath-textField');
+        expect(filepath.length).toBe(1);
+    });
+    
+    test('render defaultContent-textField', () => {
+        const wrapper = setupWrapper();
+        const defaultContent = findByAttribute(wrapper, 'defaultContent-textField');
+        expect(defaultContent.length).toBe(1);
+    });
+    
+    test('render submit-button', () => {
+        const wrapper = setupWrapper();
+        const submit = findByAttribute(wrapper, 'submit-button');
+        expect(submit.length).toBe(1);
+    });
 });
 
 // PropTypes tests
-test('PropTypes', () => {
-    const expectedProps = {
+test('FileForm PropTypes', () => {
+    const conformingProps = {
         submitText: "text",
         onSubmit: () => {},
         branch: 'branch',
         filepath: 'filepath',
         defaultContent: 'defaultContent',
     }
-    let propError = checkPropTypes(FileForm.propTypes, expectedProps, 'prop', FileForm.name);
-    expect(propError).toBeUndefined();
+    checkProps(FileForm, conformingProps)
 });
 
 
