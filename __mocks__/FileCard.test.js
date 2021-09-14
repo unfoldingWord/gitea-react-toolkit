0aadb90673d9f9d0064a507a18d96a9d691578b8
rady-ben <radhouanebenbrahim@gmail.com>
2021-09-14 22:02:43 +0100

    return the tests removed by mistake

--- __mocks__/FileCard.test.js
+++ __mocks__/FileCard.test.js
@@ -50,6 +50,83 @@ const setupWrapper = (props = {}) => {
     return shallow(<FileCard {...setupProps}/>)
 }
 
+
+test('FileForm PropTypes', () => {
+    const conformingProps = {
+        repository: {
+            owner: {
+                username: 'username',
+                avatar_url: 'avatar_url',
+            },
+            name: 'name',
+            avatar_url: 'avatar_url',
+            permissions: {
+                push: true,
+            },
+            full_name: 'full_name',
+            default_branch: 'default_branch'
+        },
+        file:{
+            name: 'file.name',
+            path: 'file.path',
+            sha: 'file.sha',
+            content: 'file.content',
+            branch: 'file.branch',
+            filepath: 'filepath',
+            save: () => {},
+            dangerouslyDelete: () => {},
+            close: () => {}
+        },
+        isAuthenticated: true,
+    }
+    checkProps(FileCard, conformingProps);
+});
+
+// render tests
+describe('render fileCard elements',() => {
+    test('render fileCard', () => {
+        const wrapper = setupWrapper();
+        const fileCard = findByAttribute(wrapper, 'component-fileCard');
+        expect(fileCard.length).toBe(1);
+    });
+
+    test('render cardHeader', () => {
+        const wrapper = setupWrapper();
+        const cardHeader = findByAttribute(wrapper, 'cardHeader');
+        expect(cardHeader.length).toBe(1);
+    });
+
+    test('render blockEditable', () => {
+        const wrapper = setupWrapper();
+        const blockEditable = findByAttribute(wrapper, 'blockEditable');
+        expect(blockEditable.length).toBe(1);
+    });
+
+    test('render previewButton', () => {
+        const wrapper = setupWrapper();
+        const previewButton = findByAttribute(wrapper, 'previewButton');
+        expect(previewButton.length).toBe(1);
+    });
+
+    test('render saveButton', () => {
+        const wrapper = setupWrapper();
+        const saveButton = findByAttribute(wrapper, 'saveButton');
+        expect(saveButton.length).toBe(1);
+    });
+
+    test('render deleteButton', () => {
+        const wrapper = setupWrapper();
+        const deleteButton = findByAttribute(wrapper, 'deleteButton');
+        expect(deleteButton.length).toBe(1);
+    });
+
+    test('render closeButton', () => {
+        const wrapper = setupWrapper();
+        const closeButton = findByAttribute(wrapper, 'closeButton');
+        expect(closeButton.length).toBe(1);
+    });
+});
+
 describe('switch the preview state',() => {    
     test('switch the preview state from true to false', () => {
         useState.mockReturnValue([true, mockSetPreview]);
