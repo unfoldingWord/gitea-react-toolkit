f432d9b3040d3b96ba140573224cff36cd9cabb0
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2026-04-14 14:38:15 -0400

    added diff module support

--- src/components/file/helpers.js
+++ src/components/file/helpers.js
@@ -1,3 +1,4 @@
+import { applyPatch, createPatch } from 'diff'
 import {
   get, updateContent, ensureContent, deleteContent, decodeBase64ToUtf8, createContent,
 } from '../..';
@@ -162,3 +163,45 @@ export const manifestFileComparer = ({
 
   return compare;
 };
+
+/**
+ * replace any characters that are not compatible with a json string content
+ * @param {string} diffFile
+ * @return {string} escaped text
+ */
+export function escapeJsonStringChars(diffFile) {
+  let newDiff = diffFile.replaceAll('\\', '\\\\')
+  newDiff = newDiff.replaceAll('\n\r', '\\n')
+  newDiff = newDiff.replaceAll('\n', '\\n')
+  newDiff = newDiff.replaceAll('"', '\\"')
+  return newDiff
+}
+
+/**
+ * create a patch of the differences between the contents of originalFile and editedFile
+ * @param {string} fileName - name of the file being changed
+ * @param {string} originalFileContents - original file contents
+ * @param {string} editedFileContents - new file contents
+ * @param {boolean} jsonEscape - if true then we escape characters to fit in json string contents
+ * @param {number} contextLines - number of context lines to include in patch
+ * @return {string} - differences between files
+ */
+export function getPatch(fileName, originalFileContents, editedFileContents, jsonEscape = false, contextLines = 4) {
+  let diffResult = createPatch(fileName, originalFileContents, editedFileContents, undefined, undefined, { context: contextLines })
+
+  if (jsonEscape) {
+    diffResult = escapeJsonStringChars(diffResult)
+  }
+  return diffResult
+}
+
+/**
+ *
+ * @param {string} originalFileContents - original file contents
+ * @return {string} - updated file contents
+ */
+export function applyPatchToString(originalText, diff) {
+  let newContents = applyPatch(originalText, diff)
+  return newContents
+}
+
--- src/components/file/index.js
+++ src/components/file/index.js
@@ -2,4 +2,5 @@ export { default as FileCard } from './FileCard';
 export { default as FileForm } from './FileForm';
 export { default as useFile } from './useFile';
 export { default as useEdit } from './useEdit';
+export * from './helpers';
 export * from './File.context';
